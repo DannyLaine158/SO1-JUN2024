4c672d7495a9088e03b07e818954e3854f95fa6d
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2024-06-12 22:32:58 -0600

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,55 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {getRam} from "./api/Endpoint.jsx";
+import {createProcess, deleteProcess, getRam} from "./api/Endpoint.jsx";
 
 function App() {
 
-    const [ percentRam, setPercentRam ] = useState("");
+    const [ percentRam, setPercentRam ] = useState(0);
+    const [ actionButton, setActionButton ] = useState("Crear Proceso");
+    const [ pid, setPid ] = useState(0);
 
-    const insertPercentRam = async () => {
-        const req = await getRam();
-        const res = await req.json();
-        console.log(res);
-        setPercentRam(res);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            (
+                async () => {
+                    try {
+                        const req = await getRam();
+                        const res = await req.json();
+                        console.log(res);
+                        setPercentRam(res.percent);
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            )();
+        }, 500);
+
+        return () => clearInterval(interval);
+
+    }, [percentRam]);
+
+    const createProc = async () => {
+        try {
+            const req = await createProcess();
+            const res = await req.json();
+            setPid(res.pid);
+            setActionButton("Matar Proceso");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const deleteProc = async () => {
+        try {
+            const req = await deleteProcess(pid);
+            const res = await req.json();
+            console.log(res);
+            setActionButton("Crear Proceso");
+        } catch (e) {
+            console.log(e);
+        }
     }
 
   return (
@@ -25,16 +62,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>RAM: {percentRam}</h1>
+      <h1>RAM: {percentRam}%</h1>
       <div className="card">
-        <button onClick={insertPercentRam}>
-            Presiona para ver RAM
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+          { actionButton === "Crear Proceso" ? (
+                  <button onClick={createProc}>
+                      { actionButton }
+                  </button>
+              )
+              : (
+                  <button onClick={deleteProc}>
+                      {actionButton}
+                  </button>
+              )}
+          <p>
+              {pid}
+          </p>
       </div>
-      <p className="read-the-docs">
+        <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
--- frontend/src/api/Endpoint.jsx
+++ frontend/src/api/Endpoint.jsx
@@ -1,8 +1,22 @@
 // const url = "http://localhost:8000";
 
 export function getRam() {
-    return fetch(`/insertRam`, {
+    return fetch(`/getRam`, {
         headers: {'Content-Type': 'application/json'},
         method: 'GET',
     });
+}
+
+export function createProcess() {
+    return fetch(`/insertProcess`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    })
+}
+
+export function deleteProcess(pid) {
+    return fetch(`/delProcess?pid=${pid}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    })
 }
