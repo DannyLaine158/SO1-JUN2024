7a1f0d34eec68ee14ca6e4c80ae04aceb2d09cc2
Daniel Velásquez <141380631+DannyLaine158@users.noreply.github.com>
2024-06-27 20:15:59 -0600

    Upload client for rust

--- gRPC/clientGRPC/Main.go
+++ gRPC/clientGRPC/Main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	pb "clientGRPC/client"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"io"
 	"log"
+	"net/http"
 )
 
 var ctx = context.Background()
@@ -17,7 +21,30 @@ type Data struct {
 	Pais  string
 }
 
+func sendToRust(data *Data) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:8000/set", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(res.Body)
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+}
+
 func sendData(c *fiber.Ctx) error {
+	/* API REST */
 	var data map[string]string
 	e := c.BodyParser(&data)
 	if e != nil {
@@ -29,6 +56,14 @@ func sendData(c *fiber.Ctx) error {
 		Pais:  data["pais"],
 	}
 
+	go sendGrpcServer(tweet)
+	go sendToRust(&tweet)
+
+	return nil
+}
+
+func sendGrpcServer(tweet Data) {
+	/* GRPC Client */
 	conn, err := grpc.Dial("localhost:3001", grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock())
 	if err != nil {
@@ -48,12 +83,10 @@ func sendData(c *fiber.Ctx) error {
 		Pais:  tweet.Pais,
 	})
 	if err != nil {
-		return err
+		log.Fatal(err)
+	} else {
+		fmt.Println("Respuesta del servidor ", ret)
 	}
-
-	fmt.Println("Respuesta del servidor ", ret)
-
-	return nil
 }
 
 func main() {
